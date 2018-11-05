4f928b798660ce7d5eb79d6369d145f05f21c237
DESKTOP-KARS9C3\IGAW <a01097672147@gmail.com>
2018-11-05 15:47:12 +0900

    no message

--- application/views/todo_main/list_v.php
+++ application/views/todo_main/list_v.php
@@ -47,7 +47,7 @@ foreach ($list as $lt)
                             <th scope="row">
                                 <?php echo $lt->id;?>
                             </th>
-                            <td><a rel="external" href="/main/view/<?php echo $lt->id;?>"><?php echo $lt->content;?></a></td>
+                            <td><a rel="external" href="/todo_main/view/<?php echo $lt->id;?>"><?php echo $lt->content;?></a></td>
                             <td><time datetime="<?php echo mdate("%Y-%M-%j", human_to_unix($lt->created_on));?>"><?php echo $lt->created_on;?></time></td>
                             <td><time datetime="<?php echo mdate("%Y-%M-%j", human_to_unix($lt->due_date));?>"><?php echo $lt->due_date;?></time></td>
                         </tr>
@@ -57,7 +57,7 @@ foreach ($list as $lt)
                     </tbody>
                     <tfoot>
                         <tr>
-                            <th colspan="4"><a href="/main/write/" class="btn btn-success">쓰기</a></th>
+                            <th colspan="4"><a href="/todo_main/write/" class="btn btn-success">쓰기</a></th>
                         </tr>
                     </tfoot>
                 </table>
