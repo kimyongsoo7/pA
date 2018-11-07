dc8e8d93212560cd96303ca3c4ad2cbbbf984012
DESKTOP-KARS9C3\IGAW <a01097672147@gmail.com>
2018-11-07 16:33:15 +0900

    header_v , footer_v 파일들 수정

---
+++ application/views/footer_v.php
@@ -0,0 +1,11 @@
+
+    <footer id="footer">
+        <blockquote>
+            <p><a class="azubu" href="http://www.cikorea.net/" target="blank">사용자포럼</a>
+        </blockquote>
+    </footer>
+
+</div>
+
+</body>
+</html>
